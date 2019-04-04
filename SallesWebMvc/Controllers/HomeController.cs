579772e388fcd1bef67de52f2dd3e6f77b492179
Mykson Bandeira <mykson.banner@gmail.com>
2019-04-04 15:25:47 -0300

    Fist controller and Razor pages tests

--- SallesWebMvc/Controllers/HomeController.cs
+++ SallesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SallesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles web MVC App from C# Course";
+            ViewData["Aluno"] = "Mykson Wyrlley Bandeira";
 
             return View();
         }
